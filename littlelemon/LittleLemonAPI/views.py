6c65702b71a5708197867031f66818febc70692b
em4n0n <58372945+em4n0n@users.noreply.github.com>
2025-04-06 14:49:39 -0700

    create a view to use get and post methods to the MenuItems

--- littlelemon/LittleLemonAPI/views.py
+++ littlelemon/LittleLemonAPI/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 from rest_framework import generics
+from .serializers import MenuItemSerializer
+from .models import MenuItem
 
 # Create your views here.
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    
