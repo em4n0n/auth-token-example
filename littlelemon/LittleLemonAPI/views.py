d5851df650d3f38a328445d27c1872943073647d
em4n0n <58372945+em4n0n@users.noreply.github.com>
2025-04-06 15:25:15 -0700

    import permission classes and authentication

--- littlelemon/LittleLemonAPI/views.py
+++ littlelemon/LittleLemonAPI/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import MenuItemSerializer
 from .models import MenuItem
-
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticaed
 # Create your views here.
 
 class MenuItemView(generics.ListCreateAPIView):
